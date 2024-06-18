1ce55c19f628daabd83dd8a3ef50db858f0e671e
David Marom <davidmarom@hotmail.com>
2024-06-18 23:31:27 +0300

    wip

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const cors = require('cors'); // Import the cors package
+
+const app = express();
+app.use(cors()); // Enable CORS
+
+const server = http.createServer(app);
+const io = socketIo(server, { cors: { origin: '*', methods: ['GET', 'POST'] } });
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+
+    socket.on('disconnect', () => { console.log('user disconnected') });
+
+    socket.on('message', (message) => {
+        console.log('message received:', message);
+        io.emit('message', message);
+    });
+});
+
+server.listen(3001, () => { console.log('listening on *:3001') });
